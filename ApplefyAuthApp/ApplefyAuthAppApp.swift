658d28593b3c597b0e37965a42150eb1449de481
Denys Havryliak <den4iklvivua@gmail.com>
2023-05-25 15:44:20 +0300

    Initial Commit

---
+++ ApplefyAuthApp/ApplefyAuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplefyAuthAppApp.swift
+//  ApplefyAuthApp
+//
+//  Created by Denys on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ApplefyAuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
